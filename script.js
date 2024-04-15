50af2a6349f16c7b55310775f9e9dbffecc5b3c2
TigranMiraqyan <165404773+TigranMiraqyan@users.noreply.github.com>
2024-04-15 15:16:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ const toCurrency = document.getElementById('toCurrency');
 const amount = document.getElementById('amount');
 const result = document.getElementById('result');
 
-  fetch('https://restcountries.com/v3.1/all')
+fetch('https://restcountries.com/v3.1/all')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(country => {
+      const option = document.createElement('option');
+      option.text = `${country.name.common} (${country.currencies[Object.keys(country.currencies)[0]].name})`;
+      option.value = country.currencies[Object.keys(country.currencies)[0]].code;
+      fromCurrency.appendChild(option);
+      toCurrency.appendChild(option.cloneNode(true));
+    });
+  });
+
+function convertCurrency() {
+  const from = fromCurrency.value;
+  const to = toCurrency.value;
+  const amountValue = amount.value;
+
+  fetch(`https://restcountries.com/v3.1/currency/{currency}/${from}`)
     .then(response => response.json())
     .then(data => {
-      data.forEach(country => {
-        const option = document.createElement('option');
-        option.text = `${country.name.common} (${country.currencies[Object.keys(country.currencies)[0]].name})`;
-        option.value = country.currencies[Object.keys(country.currencies)[0]].code;
-        fromCurrency.add(option);
-        toCurrency.add(option.cloneNode(true));
-      });
+      const exchangeRate = data.rates[to];
+      result.textContent = (amountValue * exchangeRate).toFixed(2);
     });
-
-  function convertCurrency() {
-    const from = fromCurrency.value;
-    const to = toCurrency.value;
-    const amountValue = amount.value;
-    result.textContent = (amountValue * 0.75).toFixed(2); 
-  }
+};
